2ef3c3c42fba4ef90d6a2fe5960b3ffe7061e03c
abrarkhanmokashi <abrarkhanmokashi@eng.rizvi.edu.in>
2026-08-06 19:55:41 +0530

    Add Person class with display method

---
+++ Experiment3/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    int age;
+    String gender;
+
+    void display() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Gender: " + this.gender);
+    }
+}
