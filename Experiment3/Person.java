4daa9d29115cd659a83eaceb2f2e5b2d869ef83e
abrarkhanmokashi <abrarkhanmokashi@eng.rizvi.edu.in>
2026-09-07 20:00:38 +0530

    Refactor Person class to use private fields and constructor

--- Experiment3/Person.java
+++ Experiment3/Person.java
@@ -1,9 +1,15 @@
 public class Person {
-    String name;
-    int age;
-    String gender;
-
-    void display() {
+    private String name;
+    private int age;
+    private String gender;
+ 
+    Person(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+ 
+    void display(){
         System.out.println("Name: " + this.name);
         System.out.println("Age: " + this.age);
         System.out.println("Gender: " + this.gender);
