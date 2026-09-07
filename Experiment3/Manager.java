e0eca1dca6f927201c7f4914ef1d759d963c2bfd
abrarkhanmokashi <abrarkhanmokashi@eng.rizvi.edu.in>
2026-09-07 20:01:09 +0530

    Refactor Manager class with updated fields and constructor

--- Experiment3/Manager.java
+++ Experiment3/Manager.java
@@ -1,10 +1,16 @@
-public class Manager extends Employee {
-    int num_of_proj;
-    int num_of_teams;
-
-    void display() {
+public class Manager extends Employee{
+    private int number_of_projects;
+    private int number_of_team_members;
+ 
+    Manager(String name, int age, String gender, int salary, String working_hours, int employee_id, String department, int number_of_projects, int number_of_team_members){
+        super(name,age, gender, salary, working_hours, employee_id, department);
+        this.number_of_projects = number_of_projects;
+        this.number_of_team_members = number_of_team_members;
+    }
+ 
+void display(){
         super.display();
-        System.out.println("Number of Projects: " + this.num_of_proj);
-        System.out.println("Number of Teams: " + this.num_of_teams);
+        System.out.println("Number of Projects Completed: " + this.number_of_projects);
+        System.out.println("Number of Members in Team: " + this.number_of_team_members);
     }
 }
